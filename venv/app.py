e715679bd6f0ac0066a4dfa2e594224ba9a78747
Sebastian Lehto <sebastian.m.lehto@utu.fi>
2022-09-26 00:50:20 +0300

    Ensimmäinen versio

---
+++ venv/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/main", methods=["POST"])
+def main():
+    return render_template("main.html")

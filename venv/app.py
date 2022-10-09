0bd33fc00ef6ee67ad04d1e8ee1f9a00bce2a822
sebastian-lehto <sebastian.lehto@gmail.com>
2022-10-10 01:15:04 +0300

    toinen versio

--- venv/app.py
+++ venv/app.py
@@ -1,13 +1,33 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request, session
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy 
+
 
 app = Flask(__name__)
+app.secret_key = getenv(SECTER_KEY)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres"
+db = SQLAlchemy(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/main", methods=["POST"])
+@app.route("/uusi")
+def uusi():
+    return render_template("uusi.html")
+
+@app.route("/login")
+def login():
+    usr = request.form["tunnus"]
+    usr = request.form["salasana"]
+
+    session["username"] = usr
+    return redirect("/main")
+
+@app.route("/main")
 def main():
-    return render_template("main.html")
+    result = db.session.execute("SELECT id, name, groups, ratings, rating, des FROM restaurants")
+    rests = result.fetchall()
+    return render_template("main.html", count=len(rests), rests = rests)
